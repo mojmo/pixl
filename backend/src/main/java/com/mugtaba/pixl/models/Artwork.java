e753eca33ed1fa620e0a422683aa2381d0974518
Mugtaba Mohamed <mojtaba.mohamed187@gmail.com>
2025-09-19 16:11:48 +0300

    fix: modify 'updatedAt' format

--- backend/src/main/java/com/mugtaba/pixl/models/Artwork.java
+++ backend/src/main/java/com/mugtaba/pixl/models/Artwork.java
@@ -13,6 +13,7 @@ public class Artwork {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 
     public Artwork() {
